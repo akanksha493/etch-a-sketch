c6f0708a5bd260a2fe887edff8e3777e74c0c13f
akanksha493 <akanksha.ydv.aku@gmail.com>
2023-09-13 22:16:23 +0530

    added other functions suxh as various pens, eraser, multiple layouts

--- README.md
+++ README.md
@@ -2,8 +2,17 @@
 A javascript project based on [Etch-a-sketch](https://en.wikipedia.org/wiki/Etch_A_Sketch) toy using simple DOM manipulation. This is a project in [odin project](https://www.theodinproject.com/lessons/foundations-etch-a-sketch) curriculum.
 
 ### How It Works
-It used 16x16 grid layout, and wherever you hover over a pixel it gets colored(black). Reset button reset the grids, basically erasing all the changes.
 
-[website-link](https://akanksha493.github.io/etch-a-sketch/) 
+It is basically a sketch pad that contains grids, and on hover, it colors them.
+Some of the functionality it contains are:
+* Grid Layout : Lets you set various layouts such as 16x16, 32x32 and 100x100
+* Color Pen: Lets you choose various color for pen.
+* Pen : Normal pen it draws as a black color pen.
+* Eraser : It erases whichever grid is filled, also works by hovering.
+* Shading Pen: It is basically a pen but it is progressive darkening, meaning that for every empty cell/grid it succesesively add 10% of black color on hover.
+* Reset : This resets your sketch pad, clearing all colored grid(s)/cell(s).
 
-![screenshot](./images/screenshot.png)
+
+Live-preview : [website-link](https://akanksha493.github.io/etch-a-sketch/) 
+
+![screenshot](./images/screenshot2.png)
--- script.js
+++ script.js
@@ -1,33 +1,106 @@
 
 
+
 const gridContainer = document.querySelector(".grid-container");
 
 function createGrids(gridSize){
+    while(gridContainer.firstChild){
+        gridContainer.removeChild(gridContainer.lastChild);
+    }
     const grid = document.createElement("div");
     grid.classList.add("grid");
+    grid.style.opacity = "0";
+    // grid.style.width = (320/gridSize)+"px";
+    const containerWidth = getComputedStyle(gridContainer).width;
+    grid.style.width =(parseInt(containerWidth)/gridSize)+"px";
     for(i = 0 ;i<gridSize*gridSize;i++){
        gridContainer.appendChild(grid.cloneNode(true)); 
     }
 }
 
-function fillgrid(e){
-    e.target.style.backgroundColor = "black";
-}
 
-function clearGrid(){
+
+function clearGrids(){
     const grids = document.querySelectorAll(".grid");
     grids.forEach((grid)=>{
-        grid.style.backgroundColor = "white";
+        grid.style.opacity = "0";
     });
+}
+function eraseGrid(e){
+    e.target.style.opacity = "0";
+}
+function fillGridBlack(e){
+    // console.log(e.target.style.opacity);
+    e.target.style.opacity = "1";
+    e.target.style.backgroundColor = "black";
+}
+function fillGridColor(e){
+    const color = document.querySelector("#color-selector");
+    e.target.style.opacity = "1";
+    e.target.style.backgroundColor = color.value;
+}
+function shadeMode(e){
+    e.target.style.backgroundColor = "black";
+    console.log(e.target.style.opacity);
+    if(parseInt(e.target.style.opacity)!==1){
+        console.log("inside if");
+        let newOpacity = parseFloat(e.target.style.opacity)+0.1;
+        console.log(newOpacity);
+        e.target.style.opacity = newOpacity ;
+    }
 }
 
 
+function layout(e){
+    createGrids(parseInt(e.target.value));
+}
+
 createGrids(16);
-console.log(document.querySelector(".grid"));
-const grids = document.querySelectorAll(".grid");
-grids.forEach((grid)=>{
-    grid.addEventListener('mouseover',fillgrid);
+
+
+const choiceLayout = document.querySelector("#layout-choice");
+choiceLayout.addEventListener("change",layout);
+
+const resetBttn = document.querySelector("#reset");
+resetBttn.addEventListener('click',clearGrids);
+
+const eraseBttn = document.querySelector("#eraser");
+eraseBttn.addEventListener('click',function(){
+    const grids = document.querySelectorAll(".grid");
+    grids.forEach((grid)=>{
+        grid.addEventListener('mouseover',eraseGrid);
+    });
 });
-const resetBttn = document.querySelector(".reset");
-resetBttn.addEventListener('click',clearGrid);
+
+const colorPenBttn = document.querySelector("#color-pen");
+colorPenBttn.addEventListener("click",function(){
+    const grids = document.querySelectorAll(".grid");
+    grids.forEach((grid)=>{
+        grid.addEventListener('mouseover',fillGridColor);
+    });
+});
+
+const penBttn = document.querySelector("#pen");
+penBttn.addEventListener("click",function(){
+    const grids = document.querySelectorAll(".grid");
+    grids.forEach((grid)=>{
+        grid.addEventListener('mouseover',fillGridBlack);
+    });
+});
+
+const shadingPen = document.querySelector("#shading-pen");
+shadingPen.addEventListener("click",function(){
+    const grids = document.querySelectorAll(".grid");
+    grids.forEach((grid)=>{
+        grid.removeEventListener('mouseover',fillGridBlack);
+        grid.removeEventListener('mouseover',fillGridColor);
+        grid.addEventListener('mouseover',shadeMode);
+    });
+});
+
+
+
+
+
+
 
